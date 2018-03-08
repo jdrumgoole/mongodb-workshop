ec0ebfd427036752b1bffb5494fa2d350d6d37e8
Joe Drumgoole <joe.drumgoole@mongodb.com>
2018-03-08 08:53:04 +0000

    Dump/load scripts and JSON data file

---
+++ getgroups.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/usr/local/m/versions/3.6.3/bin/mongoexport -d MUGS -c mug_groups -o mug_groups.json
---
+++ putgroups.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+i/usr/local/m/versions/3.6.3/bin/mongoimport  --db MUGS --collection groups  --file mug_groups.json
