9e5a965f8a709011899b806bab460b6dc1a16989
Joe Drumgoole <joe.drumgoole@mongodb.com>
2018-03-11 10:55:29 +0000

    Added indexes code

---
+++ mongocreateindex.js
@@ -0,0 +1,28 @@
+
+var mongodb = require( "mongodb") ;
+
+var mongoClient = mongodb.MongoClient ;
+
+const url = "mongodb://localhost:27017/MUGS" ;
+
+mongoClient.connect( url, function( err, client ) {
+    if ( err ) {
+        console.log( "Failed to connect to :", url ) ;
+        process.exit( 1 )
+    } else {
+        console.log( "Connected to:", url ) ;
+    }
+
+    const db = client.db( "MUGS")  ;
+    ;
+    cursor = db.collection('mug_groups').createIndex( { "urlname" : 1 }, function( err, res ) {
+        if ( err ) {
+            throw err ;
+        } else {
+            console.log( res );
+        }
+    });
+
+    client.close() ;
+})
+
